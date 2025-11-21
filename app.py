bda5d09dc873990b3a42db2c6516ae54dae6195a
Ashley <azhu50@stuy.edu>
2025-11-20 21:30:38 -0500

    login in app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
+# Configure session 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def home():
@@ -17,6 +22,11 @@ def search():
     
     return render_template("search.html")
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    
 
 if __name__ == '__main__':
     app.run(debug = True, host = "0.0.0.0", port = 3000)
