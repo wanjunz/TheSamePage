34e67176ab2a7f234b20646d28adf141c612e328
Ashley <azhu50@stuy.edu>
2025-12-05 22:45:17 -0500

    edited comments

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def default():
 @app.route('/add', methods=['POST'])
 def addBook():
     volumeID = request.form.get("volumeID") # book volumeID passed from search.html
-    # if volumeID is valid and 
+    # check if volumeID is valid  
     if checkVolumeID(volumeID) is not False:
         # if not already in homeBooks, insert book into homeBooks as one of currently readings
         inHomeBooks = executeSQL("SELECT * FROM homeBooks WHERE forum_id = ? AND user_id = ?", (volumeID, session["user_id"]), False)
@@ -319,53 +319,52 @@ def logout():
 @app.route("/comment", methods = ["POST"])
 def comment():
     # adds user comment to database and then shows page with it on
-    if request.method == "POST":
-        parent_id = request.form.get("parent_id")
-        forum_id = request.form.get("volumeID")
+    parent_id = request.form.get("parent_id")
+    forum_id = request.form.get("volumeID")
 
-        # retrieve info from 'upload comment' button in forum.html
-        comment = request.form.get("comment")
+    # retrieve info from 'upload comment' button in forum.html
+    comment = request.form.get("comment")
 
-        # if blank comment or forum_id, return home
-        if not comment or not forum_id: 
+    # if blank comment or forum_id, return home
+    if not comment or not forum_id: 
+        return redirect("/")
+
+    # if forum info tampered with, return home
+    forumIDArray = executeSQL("SELECT * FROM chapters WHERE forum_id = ? ", (forum_id,),False)
+    if len(forumIDArray)!=1:
+        return redirect("/")
+    forumIDArray=forumIDArray[0]
+        
+    time = datetime.now().strftime("%m/%d/%Y, %H:%M")
+    # parent_id refers to the parent reply. If empty, then it's a regular comment, not a reply to a comment
+    if not parent_id:
+        page = request.form.get("page")
+
+        # general comment, no page inputted
+        if not page:
+            percent = 'N/A'
+
+        # comment with page progress inputted
+        else:
+            pageCount = float(forumIDArray[4])
+            # caclulate percent read iff page is a number between 0 and pageCount (inclusive)
+            if not(page.isdigit() and float(page) <= pageCount):
+                return forum()
+            page = float(page)
+            percent = str(round(page * 100/pageCount))        
+        executeSQL("INSERT INTO forums(user_id, comment, time, forum_id, percentage) VALUES (?,?,?,?,?)", (session["user_id"], comment, time, forum_id, percent), True)   
+
+    # reply to a comment
+    else: 
+        # check if hidden data tampered with
+        parentComment = executeSQL("SELECT * FROM forums WHERE comment_id = ? AND forum_id = ?", (parent_id, forum_id), False)
+        if len(parentComment)!=1:
             return redirect("/")
-
-        # if forum info tampered with, return home
-        forumIDArray = executeSQL("SELECT * FROM chapters WHERE forum_id = ? ", (forum_id,),False)
-        if len(forumIDArray)!=1:
-            return redirect("/")
-        forumIDArray=forumIDArray[0]
-            
-        time = datetime.now().strftime("%m/%d/%Y, %H:%M")
-        # parent_id refers to the parent reply. If empty, then it's a regular comment, not a reply to a comment
-        if not parent_id:
-            page = request.form.get("page")
-
-            # general comment, no page inputted
-            if not page:
-                percent = 'N/A'
-
-            # comment with page progress inputted
-            else:
-                pageCount = float(forumIDArray[4])
-                # caclulate percent read iff page is a number between 0 and pageCount (inclusive)
-                if not(page.isdigit() and float(page) <= pageCount):
-                    return forum()
-                page = float(page)
-                percent = str(round(page * 100/pageCount))        
-            executeSQL("INSERT INTO forums(user_id, comment, time, forum_id, percentage) VALUES (?,?,?,?,?)", (session["user_id"], comment, time, forum_id, percent), True)   
-
-        # reply to a comment
-        else: 
-            # check if hidden data tampered with
-            parentComment = executeSQL("SELECT * FROM forums WHERE comment_id = ? AND forum_id = ?", (parent_id, forum_id), False)
-            if len(parentComment)!=1:
-                return redirect("/")
-            parentComment=parentComment[0]
-            # if parentid, forumid pair exist then add reply into forums table
-            executeSQL("INSERT INTO forums(user_id, comment, parent_id, time, forum_id, percentage) VALUES(?, ?, ?, ?, ?, ?)", (session["user_id"], comment, parent_id, time, forum_id, parentComment[5]),True)
-        # open corresponding forum
-        return forum()
+        parentComment=parentComment[0]
+        # if parentid, forumid pair exist then add reply into forums table
+        executeSQL("INSERT INTO forums(user_id, comment, parent_id, time, forum_id, percentage) VALUES(?, ?, ?, ?, ?, ?)", (session["user_id"], comment, parent_id, time, forum_id, parentComment[5]),True)
+    # open corresponding forum
+    return forum()
 
 # Delete comments
 @app.route("/deleteContribution", methods = ["POST"])
